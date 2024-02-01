af2d8b0f352a3e3d9700ae2890bf1cd4f39e8059
nishant tekwani <nishanttekwani03@gmail.com>
2024-02-01 23:59:57 +0530

    set intro python

---
+++ basics.py
@@ -0,0 +1,7 @@
+# a set is a collection ( kind of data structure ) which is unordered , unindexed . No duplicate values.
+basket = {'apple','orange', 'pear','banana'}
+print(basket)
+
+#checking
+'orange' in basket #true
+'crabgrass' in basket #false
