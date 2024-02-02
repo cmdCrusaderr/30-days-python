db6b9ed19955ed673595e640ed39d031cfe70d26
nishant tekwani <nishanttekwani03@gmail.com>
2024-02-02 20:12:03 +0530

    learnt about collection called sets

--- basics.py
+++ basics.py
@@ -1,4 +1,7 @@
-#anonymous functions
-square=lambda x:x**2
-#here x is an argument
-print(square(5))
+# a set is a collection ( kind of data structure ) which is unordered , unindexed . No duplicate values.
+basket = {'apple','orange', 'pear','banana'}
+print(basket)
+
+#checking for the elements in set
+'orange' in basket #true
+'crabgrass' in basket #false
