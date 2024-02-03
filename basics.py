0d9a7a8702f4d15eff7c4b507a1337961aef73bf
nishant tekwani <nishanttekwani03@gmail.com>
2024-02-03 06:09:08 +0530

    functions and arguments in python

--- basics.py
+++ basics.py
@@ -12,4 +12,44 @@ print(dict_example)
 print(dict_example['python']) #5
 print(sorted(dict_example)) #sorted alphabetically
 
+functions in python
+def person(name,age):
+    print(name)
+    print(age)
 
+arguments in python
+def sum(a,*b):
+    print(a)
+    print(b)
+    sum=a
+    for i in b:
+        sum=sum+i
+    
+    print(sum)
+sum(34,78,90,78)
+
+# *args -> allows you to pass non-key arguments
+# **kwargs -> allows you to pass keyword args
+def print_address(**kwargs):
+    for key , value in kwargs.items():
+        print(key)
+
+# calling the function
+print_address(street='123 false street',
+              city='Noida',
+              state='Uttar Pradesh',
+              zip='201309')
+
+# ILLUSTRATION 
+def shipping_func(*args,**kwargs):
+    for arg in args:
+     print(arg)
+     print()
+    for key , value in kwargs.items():
+        print(f"{key} , {value}")
+        print()
+        
+shipping_func("dr","spongebob0","squarepants","III",
+              street='123 fake',
+              city='noida',
+              state='Uttar Pradesh')
