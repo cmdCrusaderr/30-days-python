377348c8b361dd4b8ee04e1765bb132ef61357cf
nishant tekwani <nishanttekwani03@gmail.com>
2024-02-10 01:29:56 +0530

    exception done

--- basics.py
+++ basics.py
@@ -1,58 +1,58 @@
-# # a set is a collection ( kind of data structure ) which is unordered , unindexed . No duplicate values.
-# basket = {'apple','orange', 'pear','banana'}
-# print(basket)
+# a set is a collection ( kind of data structure ) which is unordered , unindexed . No duplicate values.
+basket = {'apple','orange', 'pear','banana'}
+print(basket)
 
-# #checking for the elements in set
-# 'orange' in basket #true
-# 'crabgrass' in basket #false
+#checking for the elements in set
+'orange' in basket #true
+'crabgrass' in basket #false
 
-# # a dictionary is a set of key : with value pairs ( given that the keys are unique )
-# dict_example={'python':5,'css':0}
-# print(dict_example)
-# print(dict_example['python']) #5
-# print(sorted(dict_example)) #sorted alphabetically
+# a dictionary is a set of key : with value pairs ( given that the keys are unique )
+dict_example={'python':5,'css':0}
+print(dict_example)
+print(dict_example['python']) #5
+print(sorted(dict_example)) #sorted alphabetically
 
-# # functions in python
-# def person(name,age):
-#     print(name)
-#     print(age)
+# functions in python
+def person(name,age):
+    print(name)
+    print(age)
 
-# # arguments in python
-# def sum(a,*b):
-#     print(a)
-#     print(b)
-#     sum=a
-#     for i in b:
-#         sum=sum+i
+# arguments in python
+def sum(a,*b):
+    print(a)
+    print(b)
+    sum=a
+    for i in b:
+        sum=sum+i
 
-#     print(sum)
-# sum(34,78,90,78)
+    print(sum)
+sum(34,78,90,78)
 
-# # *args -> allows you to pass non-key arguments
-# # **kwargs -> allows you to pass keyword args
-# def print_address(**kwargs):
-#     for key , value in kwargs.items():
-#         print(key)
+# *args -> allows you to pass non-key arguments
+# **kwargs -> allows you to pass keyword args
+def print_address(**kwargs):
+    for key , value in kwargs.items():
+        print(key)
 
-# # calling the function
-# print_address(street='123 false street',
-#               city='Noida',
-#               state='Uttar Pradesh',
-#               zip='201309')
+# calling the function
+print_address(street='123 false street',
+              city='Noida',
+              state='Uttar Pradesh',
+              zip='201309')
 
-# # ILLUSTRATION
-# def shipping_func(*args,**kwargs):
-#     for arg in args:
-#      print(arg)
-#      print()
-#     for key , value in kwargs.items():
-#         print(f"{key} , {value}")
-#         print()
+# ILLUSTRATION
+def shipping_func(*args,**kwargs):
+    for arg in args:
+     print(arg)
+     print()
+    for key , value in kwargs.items():
+        print(f"{key} , {value}")
+        print()
 
-# shipping_func("dr","spongebob0","squarepants","III",
-#               street='123 fake',
-#               city='noida',
-#               state='Uttar Pradesh')
+shipping_func("dr","spongebob0","squarepants","III",
+              street='123 fake',
+              city='noida',
+              state='Uttar Pradesh')
 
 #learning about the input / output
 import math
@@ -63,3 +63,10 @@ print(f'The value of pi is approximately {math.pi}.')
 
 #errors-and-exception - event which stops the flow of program is called exception
 #difference between exception and error - error cannot be handled
+num1=int(input("enter the number of your choice :"))
+num2=int(input("enter the second number of your choice : "))
+try:
+   div=num1/num2
+   print(div)
+except :
+    print("division by zero is not possible")
--- hangman_game.py
+++ hangman_game.py
@@ -1,1 +1,15 @@
+#hangman game :
+    #logic and help from github and google : https://gist.github.com/lupinetti/8f89e5f33750aa7c91c3
 
+#this function is an intro to the users
+def hangman_intro():
+    print("welcome to the game called hangman : ")
+    print("rules for the game is : One player thinks of a word and the other tries to guess it by suggesting the letters. The word to guess is represented by a row of dashes, giving the number of letters. If the guessing player suggests a letter which occurs in the word, the program writes it in all its correct positions.")
+
+def player1_intro(name,age): #function of the guessing player
+    print(f"Hello player ,{name}")
+    print(f"Age : {age}")
+
+def player2_intro(name,age):
+    print(f"Hello player ,{name}")
+    print(f"Age : {age}")
